e2288f41b7af799b669ec5accf3e8bc2f8962b77
Édouard Siha <edouard@edine.fr>
2022-06-27 13:23:23 +0200

    feat: implement mower.RunOn

--- domain/mower/mower.go
+++ domain/mower/mower.go
@@ -2,16 +2,18 @@ package mower
 
 import (
 	"go-playground/domain/direction"
+	"go-playground/domain/instruction"
 	"go-playground/domain/lawn"
 	"go-playground/domain/point"
 )
 
 type Mower struct {
-	position Position
+	position     Position
+	instructions []instruction.Instruction
 }
 
-func New(position Position) Mower {
-	return Mower{position}
+func New(position Position, instructions []instruction.Instruction) Mower {
+	return Mower{position, instructions}
 }
 
 func (m *Mower) TurnLeft() {
@@ -37,4 +39,26 @@ func (m *Mower) Position() point.Point {
 
 func (m *Mower) Direction() direction.Direction {
 	return m.position.direction
+}
+
+func (m *Mower) RunOn(l lawn.Lawn) {
+	for _, i := range m.instructions {
+		nextPosition := m.nextPosition(i)
+		if l.Contains(nextPosition.point) {
+			m.position = nextPosition
+		}
+	}
+}
+
+func (m *Mower) nextPosition(i instruction.Instruction) Position {
+	switch i {
+	case instruction.Advance:
+		return m.position.TranslateOnce()
+	case instruction.TurnRight:
+		return m.position.Right90Degrees()
+	case instruction.TurnLeft:
+		return m.position.Left90Degrees()
+	default:
+		panic("Not handled")
+	}
 }
--- domain/mower/mower_test.go
+++ domain/mower/mower_test.go
@@ -2,6 +2,7 @@ package mower
 
 import (
 	"go-playground/domain/direction"
+	"go-playground/domain/instruction"
 	"go-playground/domain/lawn"
 	"go-playground/domain/point"
 	"testing"
@@ -9,7 +10,7 @@ import (
 
 func TestMower_TurnLeft(t *testing.T) {
 	north := direction.North
-	mower := New(NewPosition(aPoint(), north))
+	mower := New(NewPosition(aPoint(), north), []instruction.Instruction{})
 
 	mower.TurnLeft()
 
@@ -25,7 +26,7 @@ func aPoint() point.Point {
 
 func TestMower_TurnRight(t *testing.T) {
 	west := direction.West
-	mower := New(NewPosition(aPoint(), west))
+	mower := New(NewPosition(aPoint(), west), []instruction.Instruction{})
 
 	mower.TurnRight()
 
@@ -67,11 +68,58 @@ func TestMower_Advance(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			mower := New(NewPosition(p, tt.direction))
+			mower := New(NewPosition(p, tt.direction), []instruction.Instruction{})
 			mower.Advance(lawn.Rectangular(point.New(5, 5)))
 			actual := mower.position.point
 			if actual != tt.expected {
 				t.Errorf("Expected %v, got %v", tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestMower_Run(t *testing.T) {
+	l := lawn.Rectangular(point.New(4, 4))
+
+	tests := []struct {
+		name             string
+		start            Position
+		instructions     []instruction.Instruction
+		expectedPosition Position
+	}{
+		{
+			"Advance",
+			NewPosition(point.New(3, 3), direction.North),
+			[]instruction.Instruction{instruction.Advance},
+			NewPosition(point.New(3, 4), direction.North),
+		},
+		{
+			"TurnLeft",
+			NewPosition(point.New(3, 3), direction.North),
+			[]instruction.Instruction{instruction.TurnLeft},
+			NewPosition(point.New(3, 3), direction.North.Left90Degrees()),
+		},
+		{
+			"TurnRight",
+			NewPosition(point.New(3, 3), direction.North),
+			[]instruction.Instruction{instruction.TurnRight},
+			NewPosition(point.New(3, 3), direction.North.Right90Degrees()),
+		},
+		{
+			"Remain inside lawn",
+			NewPosition(point.New(3, 3), direction.North),
+			[]instruction.Instruction{instruction.TurnRight, instruction.Advance, instruction.Advance, instruction.TurnRight},
+			NewPosition(point.New(4, 3), direction.North.Right90Degrees().Right90Degrees()),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mower := New(tt.start, tt.instructions)
+			mower.RunOn(l)
+			actual := mower.position
+			if actual != tt.expectedPosition {
+				t.Errorf("Expected %+v, got %+v", tt.expectedPosition, actual)
 			}
 		})
 	}
